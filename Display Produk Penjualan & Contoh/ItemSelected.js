9ab2a987bf7d0feea17b0db4e345f6b0b324ce08
panjidwisatrio <panjisatrio888@gmail.com>
2022-03-19 15:34:14 +0700

    Tugas Responsive

---
+++ Display Produk Penjualan & Contoh/ItemSelected.js
@@ -0,0 +1,279 @@
+function pageItemOne(page) {
+    if (page === 0) {
+        document.getElementById("page-slide-itemOne").innerHTML = "Headset Bluetooth Xiaomi Redmi Airdots pro 3 TWS Wireless Redmi Airdots Original<br>" +
+            "<br>" +
+            "Warna :<br>" +
+            "- Hitam<br>" +
+            "- Putih<br>" +
+            "- Pink<br>" +
+            "<br>" +
+            "Spesifikasi :<br>" +
+            "Earbuds:<br>" +
+            "Wireless Range:10m(in an obstacle-free environment)<br>" +
+            "Battery Capacity:43mAh(per earbud)<br>" +
+            "Input:5V==100mA<br>" +
+            "Battery Type:Lithium Polymer<br>" +
+            "Wireless connectivity: Bluetooth 5.0<br>" +
+            "Bluetooth Profiles: HFP/A2DP/ HSP/AVRCP<span id=\"dots-itemOne\">...</span><span id=\"more-itemOne\"><br>" +
+            "<br>" +
+            "Charging Case:<br>" +
+            "Input:5V==500mA<br>" +
+            "Output:5V==150mA<br>" +
+            "Charging Time:Approx.2h<br>" +
+            "Battery Capacity:300mAh<br>" +
+            "Battery Type:Lithium Polymer<br>" +
+            "Net Weight of a single earphone :about 5.0g<br>" +
+            "Net Weight of the charging box :about 35g<br>" +
+            "<br>" +
+            "Total Net Weight of the box:95g<br>" +
+            "Official Package including<br>" +
+            "x2 Earbuds L & R<br>" +
+            "x4 Earbuds Cap Cadangan<br>" +
+            "x1 Battery Case<br>" +
+            "x1 User Menu<br>" +
+            "x1 Kabel Type C<br></span>";
+    } else if (page === 1) {
+        document.getElementById("page-slide-itemOne").innerHTML = "Buyer yth, kami menyadari sebagai manusia kami tdk luput dari kesalahan. Meskipun kami sdh berusaha untuk teliti dan cermat, tetap saja pasti ada 1-2<span id=\"dots-itemOne\">...</span><span id=\"more-itemOne\"> kesalahn yg kami buat. Untuk itu, kami terbuka menerima complain dgn catatan : SOPAN<br>" +
+            "<br>" +
+            "Syarat Garansi = Rating 5<br>" +
+            "Rating dibwh 5 = Garansi Hangus<br>" +
+            "Jika ad kendala alangkah baikny di diskusikan terlebih dahulu ke seller ya<br>" +
+            "<br>" +
+            "Complain yg sopan dan disampaikan dgn bahasa yg halus akan lebih enak dibaca dan kami terima<br>" +
+            "<br>" +
+            "Kami selalu menanggapi complain dgn tangan terbuka kok..jadi mohon ikuti ketentuannya ya<br>" +
+            "<br>" +
+            "Aturan Pengembalian/Retur:<br>" +
+            "1. Pembeli mengirimkan kami foto resi pengembalian barang terlebih dahulu.<br>" +
+            "2. Setelah kami menerima barang, dan kami cek terlebih dahulu, baru akan kami kirimkan barang baru.<br>" +
+            "4. Kami tidak menerima jika setelah pembeli mengirimkan resi, langsung meminta kami juga untuk mengirimkan barang returan tanpa menunggu kami menerima barang.<br>" +
+            "(Karena sudah ada beberapa kejadian, pembeli mengatakan barang rusak dan kami memperbolehkan retur, pembeli mengirimkan foto resi dan langsung meminta kami untuk mengirimkan barang, tapi ternyata saat kami terima barangnya, hanya kotak kosong aja. Jadi intinya pembeli mendapatkan 2 barang dalam 1 transaksi).<br>" +
+            "Agar menghindari kejadian tersebut, kami minta kerjasama antara kedua belah pihak dgn baik.<br>" +
+            "Terima kasih</span>"
+    }
+}
+function pageItemTwo(page) {
+    if (page === 0) {
+        document.getElementById("page-slide-itemTwo").innerHTML = "Handphone Xiaomi Redmi 10 6/128 GB 6GB 128GB<br>" +
+            "<br>" +
+            "RAM 6GB+128 GB * Penyimpanan memori dapat diperluas hingga 512GB * RAM dapat diperluas hingga 2GBLPDDR4X +eMMC*Konfigurasi yang tersedia berbeda-beda tergantung wilayah.<br>" +
+            "<br>" +
+            "Dimensi<br>" +
+            "<br>" +
+            "Tinggi: 161,95mmLebar: 75,53mmKetebalan: 8,92mmBerat: 181g<br>" +
+            "<br>" +
+            "Layar<br>" +
+            "<br>" +
+            "Display FHD+ 6,5\"Rasio refresh: 90Hz AdaptiveSync: 45/60/90Hz2400 x 1080, 405 ppiSunlight displayMode baca 3.0Corning® Gorilla® Glass 3<span id=\"dots-itemTwo\">...</span><span id=\"more-itemTwo\"><br>" +
+            "<br>" +
+            "Prosesor<br>" +
+            "<br>" +
+            "Helio G882 x Arm Cortex-A75 @ 2GHz6 x Arm Cortex-A55 @ 1,8GHzProses manufaktur 12nm GPU Arm Mali-G52<br>" +
+            "<br>" +
+            "Baterai & Pengisian Daya<br>" +
+            "<br>" +
+            "Baterai 5.000mAh (typ)Pengisian daya cepat 18WPengisian daya terbalik dengan kabel 9WPengisi daya dengan dudukan pengisian daya 22,5WUSB-C<br>" +
+            "<br>" +
+            "Kamera<br>" +
+            "<br>" +
+            "50MPf/1,88MP kamera ultra-widef/2,2FOV 120°2MP kamera makro f/2,42MP sensor depthf/2,4Perekaman video kamera belakang 1080 p | 1920x1080 pada kecepatan 30 fps 720 p | 1280x720 pada kecepatan 30 fpsKamera depan8MP kamera depanf/2,0Perekaman video kamera depan 1080 p | 1920x1080 pada kecepatan 30 fps 720 p | 1280x720 pada kecepatan 30 fps<br>" +
+            "<br>" +
+            "Keamanan<br>" +
+            "<br>" +
+            "Sensor sidik jari di bagian sampingAI Face Unlock<br>" +
+            "<br>" +
+            "Jaringan & Konektivitas<br>" +
+            "<br>" +
+            "SIM ganda + MicroSD khususPenyimpanan dapat diperbesar hingga 512GB2G: GSM: 850 900 1800 1900 MHz3G: WCDMA:1/2/4/5/84G: LTE FDD:1/2/3/4/5/7/8/20/284G: LTE TDD:38/40/41 （B41 2535-2655 MHz</span>"
+    } else if (page === 1) {
+        document.getElementById("page-slide-itemTwo").innerHTML = "- Retur atau pengembalian harus disertai oleh video unboxing <br>" +
+            "<br>" +
+            "- Tidak ada video unboxing maka retur ditolak<span id=\"dots-itemTwo\">...</span><span id=\"more-itemTwo\"><br>" +
+            "<br>" +
+            "- Ongkir ditanggung oleh pembeli<br>" +
+            "<br>" +
+            "- Memberikan review jelek (bintang 1,2,3) maka dianggap selesai dan tidak dapat complain lagi</span>";
+    }
+}
+function pageItemThree(page) {
+    if (page === 0) {
+        document.getElementById("page-slide-itemThree").innerHTML = "spesifikasi :<br>" +
+            "Processor AMD Ryzen 3 5300U<br>" +
+            "Graphics Integrated AMD Radeon Graphics<br>" +
+            "Memory 4GB Soldered DDR4 + 4GB SO-DIMM DDR4<br>" +
+            "Storage 512GB SSD M.2 2242 PCIe NVMe 3.0x2<br>" +
+            "Display 14\" FHD (1920x1080) IPS 300nits Anti-glare, 45% NTSC<br>" +
+            "Touchscreen None<br>" +
+            "Optical None<br>" +
+            "Media Reader 4-in-1 Card Reader<br>" +
+            "Ethernet None<br>" +
+            "WLAN + Bluetooth 11ac, 2x2 + BT5.0<span id=\"dots-itemThree\">...</span><span id=\"more-itemThree\"><br>" +
+            "Case Material PC / ABS<br>" +
+            "Camera 0.3MP<br>" +
+            "Microphone 2x, Array<br>" +
+            "Color Platinum Grey, IMR<br>" +
+            "Keyboard Backlit, English<br>" +
+            "TPM FW TPM 2.0<br>" +
+            "Battery Integrated 35Wh<br>" +
+            "Power Adapter 65W Round Tip<br>" +
+            "Operating System Windows 10 Home 64, English<br>" +
+            "Bundled Software Office Home and Student 2019</span>>";
+    } else if (page === 1) {
+        document.getElementById("page-slide-itemThree").innerHTML = "KHUSUS LAPTOP/PC YANG INCLUDE MICROSOFT OFFICE HOME AND STUDENT 2019<br>" +
+            "<br>" +
+            "<br>" +
+            "<br>" +
+            "Pembeli wajib untuk mengecek kondisi fisik dulu dan aktifin windows dulu dan pastikan semuanya berjalan dengan Normal.<span id=\"dots-itemThree\">...</span><span id=\"more-itemThree\"><br>" +
+            "Jika ditemukan sesuatu yang tidak normal seperti pas nyala, layar bergaris atau cacat yg lainnya, mohon untuk TIDAK MENGAKTIFKAN MICROSOFT OFFICE HOME AND STUDENT 2019.<br> " +
+            "Jika tetap mengaktifkan MICROSOFT OFFICE HOME AND STUDENT 2019, maka perggantian unit laptop tidak bisa dilakukan. Pembeli diwajibkan untuk membawa unit Laptop/PC ke Service center sendiri.</span>";
+    }
+}
+function pageItemFour(page) {
+    if (page === 0) {
+        document.getElementById("page-slide-itemFour").innerHTML = "B170<br>" +
+            "Kemudahan plug-and-play<br>" +
+            "<br>" +
+            "KEMUDAHAN PLUG & PLAY<br>" +
+            "B170 selalu siap saat kamu memerlukannya. Colokkan saja receiver ke sebuah port USB di perangkatmu agar aktif dalam hitungan detik. Kontrol kursornya yang kecil dan mulus menjadikannya cocok untuk bekerja di ruang kecil dan meja yang penuh barang. Nikmatilah dunia wireless!<br>" +
+            "<br>" +
+            "TANGAN MANA PUN, DI MANA SAJA<br>" +
+            "B170 wireless yang ringkas ini memiliki desain yang sangat cerdas sehingga dapat dimasukkan ke tas laptop sehingga kamu bisa bekerja di mana pun, tak peduli apakah kamu kidal atau kanan.<span id=\"dots-itemFour\">...</span><span id=\"more-itemFour\"><br>" +
+            "<br>" +
+            "WIRELESS PLUG & PLAY<br>" +
+            "Kebebasan wireless hingga maksimal 10 meter dengan receiver USB yang kuat dan andal. Artinya, tak ada jeda, tanpa terputus. B170 kompatibel dengan Windows®, macOS, Chrome OS™, dan Linux®, serta langsung dapat digunakan ketika kamu mencolokkan receiver USB ke komputer ataur laptopmu.<br>" +
+            "<br>" +
+            "<br>" +
+            "DIMENSI<br>" +
+            "Mouse<br>" +
+            "Tinggi: 97,7 mm<br>" +
+            "Lebar: 61,5 mm<br>" +
+            "Tebal: 35,2 mm<br>" +
+            "Berat (dengan baterai): 70,5 g<br>" +
+            "Receiver USB<br>" +
+            "Tinggi: 6,6 mm<br>" +
+            "Lebar: 14,4 mm<br>" +
+            "Tebal: 18,7 mm<br>" +
+            "Berat: 1,8 g<br>" +
+            "PERSYARATAN SISTEM<br>" +
+            "Receiver USB<br>" +
+            "Memerlukan: port USB yang kosong<br>" +
+            "Windows 7, 8, 10 or later<br>" +
+            "macOS 10.5 or later<br>" +
+            "Chrome OS<br>" +
+            "Linux Kernel 2.6+<br>" +
+            "<br>" +
+            "SPESIFIKASI TEKNIS<br>" +
+            "Teknologi sensor<br>" +
+            "Penelusuran optik yang mulus<br>" +
+            "DPI (Min./Maks.): 1000±<br>" +
+            "Tombol<br>" +
+            "Jumlah tombol: 3 (Klik Kiri/Kanan, Klik Tengah)<br>" +
+            "Scrolling<br>" +
+            "Pengguliran baris demi baris<br>" +
+            "Scroll Wheel: Ya, 2D, optik<br>" +
+            "Baterai<br>" +
+            "Baterai: 12 bulan 6Daya tahan baterai mungkin bervariasi, berdasarkan pengguna dan komputasi.<br>" +
+            "Informasi Baterai: 1 X AA (disertakan)<br>" +
+            "Konektivitas<br>" +
+            "Jenis Koneksi: Koneksi wireless 2.4 GHz<br>" +
+            "Jangkauan wireless: 10 m 7Jangkauan wireless mungkin beragam, bergantung pada pengguna, kondisi lingkungan, dan komputasi.<br>" +
+            "Koneksi / Daya: Ya, switch on/off<\span>";
+    } else if (page === 1) {
+        document.getElementById("page-slide-itemFour").innerHTML = "Saat kita berbelanja dan produk sudah diterima tapi tipe produk tidak sesuai atau ada kerusakan, Anda langsung mengajukan complain untuk menukar produk dengan yang baru.<br>" +
+            "<br>" +
+            "Bagaimana caranya ?<br>" +
+            "<br>" +
+            "- Pilih kategori masalah terkait produk, tentukan produk yang ingin Anda komplain. Pilih komplain yang ingin Anda sampaikan. Isi juga jumlah produk yang rusak/tidak sesuai/kurang serta jelaskan masalah Anda dengan lebih detail di kolom yang sudah disediakan.<br>" +
+            "<br>" +
+            "- Lampirkan foto kondisi produk yang spesifik, seperti foto produk yang tidak sesuai, bukti berupa kemasan ketika diterima, nota, atau kondisi lain yang mendukung, kemudian pilih solusi Tukar Produk Sesuai Pesanan. Klik Komplain<span id=\"dots-itemFour\">...</span><span id=\"more-itemFour\"><br>" +
+            "<br>" +
+            "- Jika kami menyetujui solusi yang Anda ajukan, segera kirim kembali produk yang rusak/tidak sesuai dengan lengkap :<br>" +
+            "<br>" +
+            "Box packaging/kemasan seperti kondisi awal barang diterima<br>" +
+            "Ada stiker Distributed by Datascrip di box packaging/kemasan<br>" +
+            "Invoice pembelian<br>" +
+            "Aksesoris/plastik/dokumen petunjuk lain dalam kemasan<br>" +
+            "ke alamat :<br>" +
+            "<br>" +
+            "Gedung Datascrip Lantai 3.<br>" +
+            "<br>" +
+            "Jalan Selaparang Blok B-15 Kav. 9 Kemayoran, Jakarta Pusat, 10610.<br>" +
+            "<br>" +
+            "Up. Divisi Ecommerce<br>" +
+            "<br>" +
+            "- Setelah produk yang rusak/tidak sesuai diterima, kami akan melakukan pengecekan selama maksimal 1x24 jam. Jika benar produk mengalami kendala, kami akan mengirim produk baru ke alamat Anda.<br>" +
+            "<br>" +
+            " <br>" +
+            "<br>" +
+            "Proses Retur tidak akan kami proses, apabila :<br>" +
+            "<br>" +
+            "- Alasan berubah pikiran/tukar dengan tipe produk yang lain<br>" +
+            "<br>" +
+            "- Kendala/kerusakan produk karena pemakaian, seperti produk pecah, terjatuh, terendam air, produk sudah pernah dibongkar/tidak tersegel, dll.<br>" +
+            "<br>" +
+            "- Produk tidak bergaransi resmi distributor Logitech di Indonesia<br>" +
+            "<br>" +
+            "- Waktu pengajuan complain lebih dari 2x24 jam setelah notifikasi ekspedisi bahwa barang sudah diterima customer<\span>";
+    }
+}
+function pageItemFive(page) {
+    if (page === 0) {
+        document.getElementById("page-slide-itemFive").innerHTML = "Xiaomi Mi Smart Band 6 memiliki layar penuh 1,56\" berteknologi AMOLED telah disempurnakan.<br>" +
+            "<br>" +
+            "Xiaomi Mi Smart Band 6 Aplikasi ini memiliki 60+ tampilan bawaan yang dirancang khusus untuk layar penuh.<br>" +
+            "<br>" +
+            "Xiaomi Mi Smart Band 6 mencatat durasi tidur, tidur nyenyak, tidur ringan, dan tidur REM secara akurat berdasarkan perubahan denyut jantung.<br>" +
+            "<br>" +
+            "Xiaomi Mi Smart Band 6 memantau tingkat stresmu dan memberikan saran untuk meredakan dan memulihkan diri dari stres.<br>" +
+            "<br>" +
+            "Warranty 12 months<span id=\"dots-itemFive\">...</span><span id=\"more-itemFive\"><br>" +
+            "Spesifikasi<br>" +
+            "<br>" +
+            "Layar<br>" +
+            "Layar AMOLED 1,56”<br>" +
+            "Resolusi layar: 152*486<br>" +
+            "Kedalaman warna layar: 326PPI<br>" +
+            "Kecerahan layar: hingga 450nit, dapat disesuaikan<br>" +
+            "<br>" +
+            "Sensor<br>" +
+            "Sensor 6-sumbu presisi tinggi dan sensor denyut jantung PPG<br>" +
+            "Sensor 6-sumbu: Akselerometer 3-sumbu dan giroskop 3-sumbu hemat daya<br>" +
+            "Sensor denyut jantung PPG<br>" +
+            "<br>" +
+            "Masa pakai baterai<br>" +
+            "Masa pakai baterai ekstra lama 14 hari dengan pengisi daya magnetik<br>" +
+            "Metode pengisian daya: Pengisian daya magnetik<br>" +
+            "Waktu pengisian daya: ≤ 2 jam<br>" +
+            "Masa pakai baterai: ≥ 14 hari<br>" +
+            "<br>" +
+            "Olahraga<br>" +
+            "30 mode kebugaran<br>" +
+            "<br>" +
+            "Kesehatan<br>" +
+            "Pemantauan denyut jantung dan kualitas tidur dan pelacakan kesehatan perempuan<br>" +
+            "Pemantauan denyut jantung<br>" +
+            "Pelacakan kesehatan wanita<br>" +
+            "<br>" +
+            "<br>" +
+            "Dimensi dan spesifikasi<br>" +
+            "Berat bersih bodi: 12,8g<br>" +
+            "Dimensi bodi: 47,4 × 18,6 × 12,7mm<br>" +
+            "Peringkat ketahanan air: 5ATM<br>" +
+            "<br>" +
+            "Konektivitas nirkabel: Bluetooth 5.0<br>" +
+            "Panjang yang bisa diatur: 155 – 219mm<br>" +
+            "Kapasitas baterai: 125mAh<br>" +
+            "Jenis baterai: Baterai polimer lithium-ion<br>" +
+            "Material tali: TPU<br>" +
+            "Material gesper tali band: Aluminum alloy<br>" +
+            "Material pelapis layar: Kaca antigores dengan pelapis anti-sidik jari<br>" +
+            "Material casing: Plastik PC<br>" +
+            "Suhu pengoperasian: 0℃～45℃<br>" +
+            "Bahasa sistem: Mendukung bahasa Inggris, Jerman, Italia, Prancis, Spanyol, Rusia, Mandarin Tradisional, Mandarin Sederhana. Bahasa-bahasa tersebut akan diluncurkan secara berurutan.<br>" +
+            "Sistem yang didukung: Android 5.0 atau iOS 10.0 dan yang lebih baru</span>";
+    } else if (page === 1) {
+        document.getElementById("page-slide-itemFive").innerHTML = "Hi. Our lovely customer dan xiaomi fans, toko kami beroperasi pukul 9:00-17:00 WIB. Senin-Sabtu(minggu dan tanggal merah offwork).<span id=\"dots-itemFive\">...</span><span id=\"more-itemFive\">" +
+            "<br>" +
+            "Hanya terima pesanan. Pesanan yang lewat jam 17:00 akan diproses saat toko beroperasi kembali. Terimakasih</span>";
+    }
+}
---
+++ Labwork_3/labwork_3.php
@@ -0,0 +1,108 @@
+<?php
+echo "SOAL NOMOR 1<br>";
+$string = "D3 Rekayasa Perangkat Lunak Aplikasi";
+
+echo substr($string, 3) . "<br>";
+echo substr($string, -8) . "<br>";
+echo substr($string, 3, 24) . "<br><br>";
+
+echo "SOAL NOMOR 2<br>";
+$strings = array(
+    'Saya adalah Abdullah',
+    'Saya punya seekor kucing yang sangat lucu',
+    'Setiap hari ku ajak dia bermain'
+);
+
+$search = array(
+    'Abdullah',
+    'kucing',
+    'lucu',
+    'bermain'
+);
+
+$replace = array(
+    'Abdurrahman',
+    'bebek',
+    'gemuk',
+    'berenang'
+);
+
+$replaced = str_replace($search, $replace, $strings);
+
+foreach ($replaced as $key => $value) {
+    echo "$key = $value" . "<br>";
+}
+
+echo "<br>";
+
+echo "SOAL NOMOR 3<br>";
+$str = 'My Password';
+$user = 'username';
+echo sprintf("The md5 hashed password of %s: %s\n", $str, md5($str.$user));
+echo "<br>";
+echo sprintf("The sha1 hashed password of %s: %s\n", $str, sha1($str.$user));
+echo "<br>";
+echo sprintf("The bin2hex hashed password of %s: %s\n", $str, bin2hex($str.$user));
+echo "<br><br>";
+
+function primeNumber($head, $last) {
+    if ($head >= $last) {
+        echo "Error: head more than last";
+    } else {
+        $size = 0;
+        for ($head; $head <= $last; $head++) {
+            $count = 0;
+            for ($i = 1; $i <= $last; $i++) {
+                if ($head % $i == 0) {
+                    $count++;
+                }
+            }
+            if ($count == 2) {
+                $size++;
+            }
+        }
+        echo "jumlah bilangan prima: $size<br><br>";
+    }
+}
+
+echo "SOAL NOMOR 4<br>";
+primeNumber(1, 20);
+
+echo "SOAL NOMOR 5<br>";
+function primeNumberEnhanced($head, $last) {
+    $size = 0;
+    for ($i = $head; $i <= $last; $i++) {
+        if (thisPrime($i)) {
+            $size++;
+        }
+    }
+    echo "jumlah bilangan prima: " . $size . "<br><br>";
+}
+
+function thisPrime($number) {
+    if ($number == 1) {
+        return false;
+    }
+
+    if ($number == 2) {
+        return true;
+    }
+
+    $x = sqrt($number);
+    $x = floor($x);
+
+    for ($i = 2; $i <= $x; $i++) {
+        if ($number % $i == 0) {
+            break;
+        }
+    }
+
+    if ($x == $i-1) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+primeNumberEnhanced(1, 200);
+?>
