06705cd33f12d9435cea7eae5e4da657748b0770
panjidwisatrio <panjisatrio888@gmail.com>
2022-03-19 15:34:14 +0700

    Tugas Responsive

---
+++ Display Produk Penjualan & Contoh/ItemHover.js
@@ -0,0 +1,75 @@
+function itemOne() {
+    const dots = document.getElementById("dots-itemOne");
+    const moreText = document.getElementById("more-itemOne");
+    const btnText = document.getElementById("myBtn-itemOne");
+
+    if (dots.style.display === "none") {
+        dots.style.display = "inline";
+        btnText.innerHTML = "Read more";
+        moreText.style.display = "none";
+    } else {
+        dots.style.display = "none";
+        btnText.innerHTML = "Read less";
+        moreText.style.display = "inline";
+    }
+}
+function itemTwo() {
+    const dots = document.getElementById("dots-itemTwo");
+    const moreText = document.getElementById("more-itemTwo");
+    const btnText = document.getElementById("myBtn-itemTwo");
+
+    if (dots.style.display === "none") {
+        dots.style.display = "inline";
+        btnText.innerHTML = "Read more";
+        moreText.style.display = "none";
+    } else {
+        dots.style.display = "none";
+        btnText.innerHTML = "Read less";
+        moreText.style.display = "inline";
+    }
+}
+function itemThree() {
+    const dots = document.getElementById("dots-itemThree");
+    const moreText = document.getElementById("more-itemThree");
+    const btnText = document.getElementById("myBtn-itemThree");
+
+    if (dots.style.display === "none") {
+        dots.style.display = "inline";
+        btnText.innerHTML = "Read more";
+        moreText.style.display = "none";
+    } else {
+        dots.style.display = "none";
+        btnText.innerHTML = "Read less";
+        moreText.style.display = "inline";
+    }
+}
+function itemFour() {
+    const dots = document.getElementById("dots-itemFour");
+    const moreText = document.getElementById("more-itemFour");
+    const btnText = document.getElementById("myBtn-itemFour");
+
+    if (dots.style.display === "none") {
+        dots.style.display = "inline";
+        btnText.innerHTML = "Read more";
+        moreText.style.display = "none";
+    } else {
+        dots.style.display = "none";
+        btnText.innerHTML = "Read less";
+        moreText.style.display = "inline";
+    }
+}
+function itemFive() {
+    const dots = document.getElementById("dots-itemFive");
+    const moreText = document.getElementById("more-itemFive");
+    const btnText = document.getElementById("myBtn-itemFive");
+
+    if (dots.style.display === "none") {
+        dots.style.display = "inline";
+        btnText.innerHTML = "Read more";
+        moreText.style.display = "none";
+    } else {
+        dots.style.display = "none";
+        btnText.innerHTML = "Read less";
+        moreText.style.display = "inline";
+    }
+}
